84ae619a76710c700387c7d89b09bde96e37813f
Crzisaac <Crzisaac54@hotmail.com>
2025-10-03 14:37:11 -0500

    Fix Contract import + connect getMessage() with frontend

--- my-dapp/src/app/utils/contract.ts
+++ my-dapp/src/app/utils/contract.ts
@@ -1,15 +1,13 @@
-// utils/contract.ts
 import { client } from './client'
-import contractAbi from '../abi.json'
+import abi from '../abi.json'
 
 const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`
 
 export async function readMessage() {
-  const data = await client.readContract({
+  return client.readContract({
     address: contractAddress,
-    abi: contractAbi as any,
+    abi: abi as any,
     functionName: 'getMessage',
     args: [],
   })
-  return data
 }
